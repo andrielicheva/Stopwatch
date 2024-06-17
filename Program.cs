273993c6506a2d1aecbda9d4a9a63098db6585fa
Andrieli Cheva <andrielicheva@employer.com.br>
2024-06-17 13:31:03 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Cronometro iniciado, Selecione uma das opção:");
+            Console.WriteLine("");
+            Console.WriteLine("S = Segundo => Exemplo = 10s");
+            Console.WriteLine("M = Minuto => Exemplo = 10m");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+
+            string data = Console.ReadLine().ToLower().Trim();
+
+            if (data == "0")
+            {
+                Console.WriteLine("Programa encerrado.");
+                Environment.Exit(0);
+            }
+
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+        }
+        static void Start(int time)
+        {
+
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("StopWhatch finalizado");
+            Thread.Sleep(2500);
+            Menu();
+        }
+    }
+}
